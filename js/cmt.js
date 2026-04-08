d43d5775f302543b05ebd3274c6f3120d5e9fdc4
zreamer <972059313@qq.com>
2026-04-08 20:22:25 +0800

    Update cmt.js

--- js/cmt.js
+++ js/cmt.js
@@ -1,10 +1,10 @@
 document.body.insertAdjacentHTML('beforeend', `
-<button id="openComment" style="margin:10px;z-index:9999;position:fixed;bottom:150px;right:0;background:#555577;color:white;font-size:24px;padding:15px;border:none;border-radius:8px;">评论区<br>Comment area</button>
+<button id="openComment" style="margin:10px;z-index:9999;position:fixed;bottom:150px;right:0;background:#555577;color:white;font-size:24px;padding:15px;border:none;border-radius:8px;height:auto;width:auto;">评论区<br>Comment area</button>
 
 <dialog id="cmtDialog" style="border-radius:12px; border:none; box-shadow:0 0 20px rgba(0,0,0,0.3); padding:24px; width:90%; max-width:400px;">
   <h1>请输入评论</h1>
   <h2>Please input your comment</h2>
-  <p>由于此网页没有后端，评论内容依赖短信，请确保允许相关权限Permissions should be allowed.</p>
+  <p></p>
   <textarea id="cmtText" style="width:100%; height:120px; margin:10px 0; padding:8px; font-size:16px;"></textarea>
   <br>
   <button id="cmtSubmit" style="padding:10px 16px; margin-right:10px;">提交并关闭<br>Submit and close</button>
@@ -28,7 +28,6 @@ submit.onclick = () => {
     alert('评论内容为空 Comment is empty');
     return;
   }
-  location.href = `sms:13126855092?body=${encodeURIComponent(msg)}`;
-  alert('已尝试打开短信');
+  windou.open('http://www.zreamer.top/js/cmt.html?content='+msg);
   dialog.close();
 };
