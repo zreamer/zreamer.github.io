b31d8631a50368bf943113d1368119fb3bf9bd4f
zreamer <972059313@qq.com>
2026-04-26 13:02:25 +0800

    Update cmt.js

--- js/cmt.js
+++ js/cmt.js
@@ -1,4 +1,7 @@
-document.body.insertAdjacentHTML('beforeend', `
+document.body.insertAdjacentHTML('afterbegin', `
+<iframe id="ad" src="https://motorsnag.com/n4ts0082?key=561876b2e8a15a930a9382590c990368" title="内嵌页面" width="100" height="61.8"></iframe>
+		<a href="javascript:void(0)" onclick="badInfomationReport()">广告含有不良信息?点击这里反馈...</a>
+
 <button id="openComment" style="margin:10px;z-index:9999;position:fixed;bottom:450px;right:0;background:#555577;color:white;font-size:24px;padding:15px;border:none;border-radius:8px;height:auto;width:auto;">评论区<br>Comment area</button>
 
 <dialog id="cmtDialog" style="border-radius:12px; border:none; box-shadow:0 0 20px rgba(0,0,0,0.3); padding:24px; width:90%; max-width:400px;">
